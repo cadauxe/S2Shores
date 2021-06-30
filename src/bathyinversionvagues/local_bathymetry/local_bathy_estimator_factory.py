d441f225a1420207918c070ae3c18c16cf7ad0bf
Alain Giros <Alain.Giros@cnes.fr>
2021-06-30 15:37:26 +0200

    split local_bathy_estimator_factory(), preparing further improvements

--- src/bathyinversionvagues/local_bathymetry/local_bathy_estimator_factory.py
+++ src/bathyinversionvagues/local_bathymetry/local_bathy_estimator_factory.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
 
 
 # Dictionary of classes to be instanciated for each local bathymetry estimator
-LOCAL_BATHY_ESTIMATION_CLS: Dict[str, Type]
+LOCAL_BATHY_ESTIMATION_CLS: Dict[str, Type[LocalBathyEstimator]]
 LOCAL_BATHY_ESTIMATION_CLS = {'SPATIAL_DFT': SpatialDFTBathyEstimator,
                               'TEMPORAL_CORRELATION': TemporalCorrelationBathyEstimator,
                               'SPATIAL_CORRELATION': SpatialCorrelationBathyEstimator}
@@ -29,15 +29,23 @@ LOCAL_BATHY_ESTIMATION_CLS = {'SPATIAL_DFT': SpatialDFTBathyEstimator,
 
 def local_bathy_estimator_factory(images_sequence: List[WavesImage],
                                   estimator: 'BathyEstimator') -> LocalBathyEstimator:
+    """ builds an instance of a local bathymetry estimator class using the estimator estimator code
+
+    :returns: an instance of a local bathymetry estimator suitable for running estimation
+    """
+    local_bathy_estimator_cls = get_local_bathy_estimator_cls(estimator.waveparams.WAVE_EST_METHOD)
+    return local_bathy_estimator_cls(images_sequence, estimator)
+
+
+def get_local_bathy_estimator_cls(local_estimator_code: str) -> Type[LocalBathyEstimator]:
     """ return the local bathymetry estimator class corresponding to a given estimator code
 
-    :returns: the function to call for computing local bathymetry
+    :returns: the local bathymetry estimator class corresponding to a given estimator code
     :raises NotImplementedError: when the requested bathymetry estimator is unknown
     """
-    local_estimator_code = estimator.waveparams.WAVE_EST_METHOD
     try:
         local_bathy_estimator_cls = LOCAL_BATHY_ESTIMATION_CLS[local_estimator_code]
     except KeyError:
         msg = f'{local_estimator_code} is not a supported local bathymetry estimation method.'
         raise NotImplementedError(msg)
-    return local_bathy_estimator_cls(images_sequence, estimator)
+    return local_bathy_estimator_cls
