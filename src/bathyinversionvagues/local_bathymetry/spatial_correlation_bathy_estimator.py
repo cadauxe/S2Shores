0a0dba7a6aa1a13661b3151a691f610017a61f60
Romain Degoul <romain.degoul@cnes.fr>
2021-08-05 17:37:59 +0000

    EHN : removing print

--- src/bathyinversionvagues/local_bathymetry/spatial_correlation_bathy_estimator.py
+++ src/bathyinversionvagues/local_bathymetry/spatial_correlation_bathy_estimator.py
@@ -10,20 +10,12 @@ Module containing all wave parameters estimation methods
 """
 import numpy as np
 
-from ..image_processing.shoresutils import (fft_filtering, compute_sinogram,
-                                            compute_celerity, correlation_tuning,
-                                            sinogram_tuning, compute_wave_length, compute_period,
-                                            temporal_reconstruction,
-                                            temporal_reconstruction_tuning,
-                                            create_sequence_time_series_spatial,
-                                            compute_angles_distances, compute_spatial_correlation
-                                            )
+from ..image_processing.shoresutils import fft_filtering
 
 from .local_bathy_estimator import LocalBathyEstimator
 
 
 class SpatialCorrelationBathyEstimator(LocalBathyEstimator):
-
     def run(self) -> None:
         """ Run the local bathy estimator using the spatial correlation method
 
@@ -44,9 +36,10 @@ class SpatialCorrelationBathyEstimator(LocalBathyEstimator):
                                                number_frame_shift=params.TEMPORAL_LAG)
             corr_tuned = correlation_tuning(correlation_matrix=corr,
                                             ratio=params.TUNING.RATIO_SIZE_CORRELATION)
-            (sinogram_max_var, angle, variance, radon_matrix) = compute_sinogram(correlation_matrix=corr_tuned,
-                                                                                 median_filter_kernel_ratio=params.TUNING.MEDIAN_FILTER_KERNEL_RATIO_SINOGRAM,
-                                                                                 mean_filter_kernel_size=params.TUNING.MEAN_FILTER_KERNEL_SIZE_SINOGRAM)
+            (sinogram_max_var, angle, variance, radon_matrix) = compute_sinogram(
+                correlation_matrix=corr_tuned,
+                median_filter_kernel_ratio=params.TUNING.MEDIAN_FILTER_KERNEL_RATIO_SINOGRAM,
+                mean_filter_kernel_size=params.TUNING.MEAN_FILTER_KERNEL_SIZE_SINOGRAM)
             sinogram_tuned = sinogram_tuning(sinogram=sinogram_max_var,
                                              mean_filter_kernel_size=params.TUNING.MEAN_FILTER_KERNEL_SIZE_SINOGRAM)
             wave_length, zeros = compute_wave_length(sinogram=sinogram_tuned)
@@ -54,7 +47,8 @@ class SpatialCorrelationBathyEstimator(LocalBathyEstimator):
                                                 spatial_resolution=params.RESOLUTION.SPATIAL,
                                                 time_resolution=params.RESOLUTION.TEMPORAL,
                                                 temporal_lag=params.TEMPORAL_LAG)
-            SS = temporal_reconstruction(angle=angle, angles=np.degrees(angles), distances=distances, celerity=celerity,
+            SS = temporal_reconstruction(angle=angle, angles=np.degrees(angles),
+                                         distances=distances, celerity=celerity,
                                          correlation_matrix=corr,
                                          time_interpolation_resolution=params.RESOLUTION.TIME_INTERPOLATION)
             SS_filtered = temporal_reconstruction_tuning(SS,
