0927c491b1673b6c3a696876636d9c8c73578139
Alain Giros <Alain.Giros@cnes.fr>
2021-12-08 15:28:04 +0100

    regrouped providers settings into BathyEstimater

--- src/bathyinversionvagues/global_bathymetry/bathy_estimator.py
+++ src/bathyinversionvagues/global_bathymetry/bathy_estimator.py
@@ -5,13 +5,15 @@
 :created: 17/05/2021
 """
 from abc import ABC
-from typing import List, Optional, Dict, Union  # @NoMove
 from pathlib import Path
-
-from munch import Munch
+from typing import List, Optional, Dict, Union  # @NoMove
 
 
 from xarray import Dataset  # @NoMove
+
+
+from munch import Munch
+
 import numpy as np
 
 from ..data_providers.delta_time_provider import DeltaTimeProvider, NoDeltaTimeProviderError
@@ -22,7 +24,6 @@ from ..data_providers.gravity_provider import (LatitudeVaryingGravityProvider, G
 from ..image.image_geometry_types import MarginsType, PointType
 from ..image.ortho_stack import OrthoStack, FrameIdType, FramesIdsType
 from ..image.sampled_ortho_image import SampledOrthoImage
-
 from .bathy_estimator_parameters import BathyEstimatorParameters
 from .ortho_bathy_estimator import OrthoBathyEstimator
 
@@ -46,10 +47,12 @@ class BathyEstimator(ABC, BathyEstimatorParameters):
         self.ortho_stack = ortho_stack
 
         self._distoshore_provider: DisToShoreProvider
-        self.set_distoshore_provider(InfinityDisToShoreProvider())  # default DisToShoreProvider
+        # set InfinityDisToShoreProvider as default DisToShoreProvider
+        self.set_distoshore_provider(provider_info=InfinityDisToShoreProvider())
 
         self._gravity_provider: GravityProvider
-        self.set_gravity_provider(LatitudeVaryingGravityProvider())  # default GravityProvider
+        # set LatitudeVaryingGravityProvider as default GravityProvider
+        self.set_gravity_provider(provider_info=LatitudeVaryingGravityProvider())
 
         # No default DeltaTimeProvider
         self._delta_time_provider: Optional[DeltaTimeProvider] = None
@@ -181,21 +184,27 @@ class BathyEstimator(ABC, BathyEstimatorParameters):
         return self._debug_sample
 
 # ++++++++++++++++++++++++++++ External data providers +++++++++++++++++++
-    def set_distoshore_provider(self, distoshore: Union[Path, DisToShoreProvider]) -> None:
+    def set_distoshore_provider(
+            self, provider_info: Optional[Union[Path, DisToShoreProvider]] = None) -> None:
         """ Sets the DisToShoreProvider to use with this estimator
 
-        :param distoshore: Either the DisToShoreProvider to use or a path to a netCDF file
+        :param provider_info: Either the DisToShoreProvider to use or a path to a netCDF file
                            assuming a geographic NetCDF format.
         """
-        distoshore_provider: DisToShoreProvider
-        if isinstance(distoshore, Path):
-            distoshore_provider = NetCDFDisToShoreProvider(distoshore, 4326,
+        if isinstance(provider_info, DisToShoreProvider):
+            distoshore_provider = provider_info
+        elif isinstance(provider_info, Path):
+            distoshore_provider = NetCDFDisToShoreProvider(provider_info, 4326,
                                                            x_axis_label='lon',
                                                            y_axis_label='lat')
         else:
-            distoshore_provider = distoshore
+            # None or some other type, keep the current provider
+            distoshore_provider = self._distoshore_provider
+
+        # Set private attribute.
         self._distoshore_provider = distoshore_provider
-        self._distoshore_provider.client_epsg_code = self.ortho_stack.epsg_code
+        if self._distoshore_provider is not None:
+            self._distoshore_provider.client_epsg_code = self.ortho_stack.epsg_code
 
     def get_distoshore(self, point: PointType) -> float:
         """ Provides the distance from a given point to the nearest shore.
@@ -205,26 +214,32 @@ class BathyEstimator(ABC, BathyEstimatorParameters):
         """
         return self._distoshore_provider.get_distoshore(point)
 
-    def set_gravity_provider(self, gravity_provider: Union[str, GravityProvider]) -> None:
+    def set_gravity_provider(self,
+                             provider_info: Optional[Union[str, GravityProvider]] = None) -> None:
         """ Sets the GravityProvider to use with this estimator .
 
-        :param gravity_provider: an instance of GravityProvider or the name of a well known gravity
-                                 provider to use.
+        :param provider_info: an instance of GravityProvider or the name of a well known gravity
+                              provider to use. If None the current provider is left unchanged.
         :raises ValueError: when the gravity provider name is unknown
         """
-        my_gravity_provider: GravityProvider
-        if isinstance(gravity_provider, str):
-            if gravity_provider.upper() not in ['CONSTANT', 'LATITUDE_VARYING']:
-                raise ValueError('Gravity provider type unknown : ', gravity_provider)
+        if isinstance(provider_info, GravityProvider):
+            gravity_provider = provider_info
+        elif isinstance(provider_info, str):
+            if provider_info.upper() not in ['CONSTANT', 'LATITUDE_VARYING']:
+                raise ValueError('Gravity provider type unknown : ', provider_info)
             # No need to set LatitudeVaryingGravityProvider as it is the BathyEstimator default.
-            if gravity_provider.upper() == 'CONSTANT':
-                my_gravity_provider = ConstantGravityProvider()
+            if provider_info.upper() == 'CONSTANT':
+                gravity_provider = ConstantGravityProvider()
             else:
-                my_gravity_provider = LatitudeVaryingGravityProvider()
+                gravity_provider = LatitudeVaryingGravityProvider()
         else:
-            my_gravity_provider = gravity_provider
-        self._gravity_provider = my_gravity_provider
-        self._gravity_provider.client_epsg_code = self.ortho_stack.epsg_code
+            # None or some other type, keep the current provider
+            gravity_provider = self._gravity_provider
+
+        # Set private attribute.
+        self._gravity_provider = gravity_provider
+        if self._gravity_provider is not None:
+            self._gravity_provider.client_epsg_code = self.ortho_stack.epsg_code
 
     def get_gravity(self, point: PointType, altitude: float = 0.) -> float:
         """ Returns the gravity at some point expressed by its X, Y and H coordinates in some SRS,
@@ -236,21 +251,27 @@ class BathyEstimator(ABC, BathyEstimatorParameters):
         """
         return self._gravity_provider.get_gravity(point, altitude)
 
-    def set_delta_time_provider(self, delta_time_provider: Union[Path, DeltaTimeProvider]) -> None:
+    def set_delta_time_provider(
+            self, provider_info: Optional[Union[Path, DeltaTimeProvider]] = None) -> None:
         """ Sets the DeltaTimeProvider to use with this estimator.
 
-        :param delta_time_provider: Either the DeltaTimeProvider to use or a path to a file or a
-                                    directory to ba used by the associated OrthoStack to build its
-                                    provider.
+        :param provider_info: Either the DeltaTimeProvider to use or a path to a file or a
+                              directory to ba used by the associated OrthoStack to build its
+                              provider, or None to leave the provider unchanged.
         """
-        my_delta_time_provider: DeltaTimeProvider
-        if isinstance(delta_time_provider, Path):
-            my_delta_time_provider = \
-                self.ortho_stack.create_delta_time_provider(delta_time_provider)
+        delta_time_provider: Optional[DeltaTimeProvider]
+        if isinstance(provider_info, DeltaTimeProvider):
+            delta_time_provider = provider_info
+        elif isinstance(provider_info, Path):
+            delta_time_provider = self.ortho_stack.create_delta_time_provider(provider_info)
         else:
-            my_delta_time_provider = delta_time_provider
-        self._delta_time_provider = my_delta_time_provider
-        self._delta_time_provider.client_epsg_code = self.ortho_stack.epsg_code
+            # None or some other type, keep the current provider
+            delta_time_provider = self._delta_time_provider
+
+        # Set private attribute.
+        self._delta_time_provider = delta_time_provider
+        if self._delta_time_provider is not None:
+            self._delta_time_provider.client_epsg_code = self.ortho_stack.epsg_code
 
     def get_delta_time(self, first_frame_id: FrameIdType, second_frame_id: FrameIdType,
                        point: PointType) -> float:
