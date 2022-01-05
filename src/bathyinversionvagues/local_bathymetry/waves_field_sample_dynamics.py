419e87ed6670af2c0695954932965cf9a1e4dbac
Alain Giros <Alain.Giros@cnes.fr>
2022-01-05 11:55:58 +0100

    created wavelength_has_changed() callback and set_missing_attribute()

--- src/bathyinversionvagues/local_bathymetry/waves_field_sample_dynamics.py
+++ src/bathyinversionvagues/local_bathymetry/waves_field_sample_dynamics.py
@@ -26,44 +26,62 @@ class WavesFieldSampleDynamics(WavesFieldSampleGeometry):
         super().__init__()
         self._period = np.nan
         self._celerity = np.nan
-        self.register_wavelength_change(self.ensure_physical_consistency)
+        self.register_wavelength_change(self.wavelength_has_changed)
 
-    def ensure_physical_consistency(self) -> None:
-        if np.isnan(self.wavelength):
+    def wavelength_has_changed(self) -> None:
+        """ When wavelength has changed (new value is ensured to be different from the previous one)
+        either reset period and celerity if both were set, or update one of them if the other is set
+        """
+        if not np.isnan(self.period) and not np.isnan(self.celerity):
             self._period = np.nan
             self._celerity = np.nan
-        else:
-            if not np.isnan(self.celerity):
-                if np.isnan(self.period):
-                    self._period = self.wavelength / self.celerity
-                else:
-                    self._period = np.nan
-                    self._celerity = np.nan
-            else:
-                if not np.isnan(self.period):
-                    self._celerity = self.wavelength / self.period
+        self.set_missing_attribute()
+
+    def set_missing_attribute(self) -> None:
+        """Set the missing attribute among wavelength, celerity and period when 2 of them are
+        defined.
+        """
+        wavelength_set = not np.isnan(self.wavelength)
+        period_set = not np.isnan(self.period)
+        celerity_set = not np.isnan(self.celerity)
+        if wavelength_set and period_set and not celerity_set:
+            self._celerity = self.wavelength / self.period
+        elif wavelength_set and not period_set and celerity_set:
+            self._period = self.wavelength / self.celerity
+        elif not wavelength_set and period_set and celerity_set:
+            self.wavelength = self.celerity * self.period
 
     @property
     def period(self) -> float:
-        """ :returns: The waves field period (s) """
+        """ :returns: The waves field period (s), which was either externally provided or computed
+        from the wavelength and the celerity
+        """
         return self._period
 
     @period.setter
     def period(self, value: float) -> None:
-        self._period = value
-        self.ensure_physical_consistency()
+        if value != self._period:
+            self._period = value
+            if not np.isnan(self.celerity) and not np.isnan(self.wavelength):
+                self._celerity = np.nan
+                self.wavelength = np.nan
+            self.set_missing_attribute()
 
     @property
     def celerity(self) -> float:
-        """ :returns: The waves field velocity (m/s) either the celerity which was directly set or
-                      computed from the wavelength and the period
+        """ :returns: The waves field velocity (m/s), which was either externally provided or
+        computed from the wavelength and the period
         """
         return self._celerity
 
     @celerity.setter
     def celerity(self, value: float) -> None:
-        self._celerity = value
-        self.ensure_physical_consistency()
+        if value != self.celerity:
+            self._celerity = value
+            if not np.isnan(self.period) and not np.isnan(self.wavelength):
+                self._period = np.nan
+                self.wavelength = np.nan
+            self.set_missing_attribute()
 
     def __str__(self) -> str:
         result = WavesFieldSampleGeometry.__str__(self)
