30db0e2785517797c7a6fefe89cce126f2166be4
Alain Giros <Alain.Giros@cnes.fr>
2022-01-17 13:55:05 +0100

    introduced metrics shortcut

--- src/bathyinversionvagues/bathy_debug/temporal_correlation_bathy_estimator_debug.py
+++ src/bathyinversionvagues/bathy_debug/temporal_correlation_bathy_estimator_debug.py
@@ -36,14 +36,15 @@ class TemporalCorrelationBathyEstimatorDebug(LocalBathyEstimatorDebug,
         wave_celerity = wave_estimation.celerity
         wave_period = wave_estimation.period
 
+        metrics = self.metrics
         # Note that wave direction is clockwise origin east
         px = np.cos(np.deg2rad(wave_direction))
         py = -np.sin(np.deg2rad(wave_direction))
         first_image = self.images_sequence[0].pixels
         correlation_matrix = self.correlation_image.pixels
-        sinogram_max_var = self.metrics['sinogram_max_var']
-        x = self.metrics['x']
-        interval = self.metrics['interval']
+        sinogram_max_var = metrics['sinogram_max_var']
+        x = metrics['x']
+        interval = metrics['interval']
         debug_path = self.global_estimator.debug_path
 
         fig = plt.figure(constrained_layout=True)
@@ -71,7 +72,7 @@ class TemporalCorrelationBathyEstimatorDebug(LocalBathyEstimatorDebug,
 
         # Third diagram : Radon transform & maximum variance
         ax3 = fig.add_subplot(gs[1, :2])
-        radon_array, _ = self.metrics['radon_transform'].get_as_arrays()
+        radon_array, _ = metrics['radon_transform'].get_as_arrays()
         s1, _ = radon_array.shape
         directions = self.selected_directions
         d1 = np.min(directions)
@@ -79,8 +80,8 @@ class TemporalCorrelationBathyEstimatorDebug(LocalBathyEstimatorDebug,
         ax3.imshow(radon_array, interpolation='nearest', aspect='auto',
                    origin='lower', extent=[d1, d2, 0, s1])
         (l1, l2) = np.shape(radon_array)
-        plt.plot(self.selected_directions, l1 * self.metrics['variances'] /
-                 np.max(self.metrics['variances']), 'r')
+        plt.plot(self.selected_directions, l1 * metrics['variances'] /
+                 np.max(metrics['variances']), 'r')
         ax3.arrow(wave_direction, 0, 0, l1)
         plt.annotate('%d °' % wave_direction, (wave_direction + 5, 10), color='orange')
         plt.title('Radon matrix')
@@ -94,10 +95,10 @@ class TemporalCorrelationBathyEstimatorDebug(LocalBathyEstimatorDebug,
                     mpl.rcParams['lines.markersize'], c='orange')
         min_limit_x = np.min(x)
         min_limit_y = np.min(sinogram_max_var)
-        ax4.plot(x[self.metrics['wave_length_zeros']],
-                 sinogram_max_var[self.metrics['wave_length_zeros']], 'ro')
-        ax4.plot(x[self.metrics['max_indices']],
-                 sinogram_max_var[self.metrics['max_indices']], 'go')
+        ax4.plot(x[metrics['wave_length_zeros']],
+                 sinogram_max_var[metrics['wave_length_zeros']], 'ro')
+        ax4.plot(x[metrics['max_indices']],
+                 sinogram_max_var[metrics['max_indices']], 'go')
 
         bathy = depth_from_dispersion(1 / wave_estimation.wavelength,
                                       wave_estimation.celerity, self.gravity)
@@ -107,9 +108,9 @@ class TemporalCorrelationBathyEstimatorDebug(LocalBathyEstimatorDebug,
         # Fifth  diagram
         ax5 = fig.add_subplot(gs[3, :2])
         ax5.axis('off')
-        dephasings = self.metrics['dephasings']
-        celerities = self.metrics['celerities']
-        celerities_from_periods = self.metrics['celerities_from_periods']
+        dephasings = metrics['dephasings']
+        celerities = metrics['celerities']
+        celerities_from_periods = metrics['celerities_from_periods']
         chain_dx = ' '.join([f'{dephasing:.2f} | ' for dephasing in dephasings])
         chain_celerities = ' '.join([f'{celerity:.2f} | ' for celerity in celerities])
         chain_celerities_from_period = ' '.join(
@@ -120,14 +121,14 @@ class TemporalCorrelationBathyEstimatorDebug(LocalBathyEstimatorDebug,
 
         # sixth  diagram : Temporal reconstruction
         fig_temporal_signals = plt.figure('Signaux temporal', constrained_layout=True)
-        hops_number = len(self.metrics['temporal_signals'])
+        hops_number = len(metrics['temporal_signals'])
         gs = gridspec.GridSpec(hops_number, 1, figure=fig_temporal_signals)
         for i in range(hops_number):
-            temporal_signal = self.metrics['temporal_signals'][i]
-            arg_peak_max = self.metrics['arg_peaks_max'][i]
-            dephasing = self.metrics['dephasings'][i]
-            temporal_period = self.metrics['periods'][i]
-            celerities_from_periods = self.metrics['celerities_from_periods'][i]
+            temporal_signal = metrics['temporal_signals'][i]
+            arg_peak_max = metrics['arg_peaks_max'][i]
+            dephasing = metrics['dephasings'][i]
+            temporal_period = metrics['periods'][i]
+            celerities_from_periods = metrics['celerities_from_periods'][i]
             ax = fig_temporal_signals.add_subplot(gs[i, :])
             ax.plot(temporal_signal)
             ax.plot(arg_peak_max, temporal_signal[arg_peak_max], 'ro')
