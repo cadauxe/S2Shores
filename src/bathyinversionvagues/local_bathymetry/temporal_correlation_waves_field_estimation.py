63fe82a48bd99c88d4082cb335e1605998d6ad82
Alain Giros <Alain.Giros@cnes.fr>
2022-03-18 16:57:33 +0100

    Added propagated_distance property in WavesFieldEstimation and used it

--- src/bathyinversionvagues/local_bathymetry/temporal_correlation_waves_field_estimation.py
+++ src/bathyinversionvagues/local_bathymetry/temporal_correlation_waves_field_estimation.py
@@ -7,8 +7,6 @@
 :license: see LICENSE file
 :created: 10 sep 2021
 """
-import numpy as np
-
 from ..data_model.waves_field_estimation import WavesFieldEstimation
 
 
@@ -16,17 +14,3 @@ class TemporalCorrelationWavesFieldEstimation(WavesFieldEstimation):
     """ This class encapsulates the information estimated in a waves field sample by a
     TemporalCorrelationBathyEstimator.
     """
-
-    def __init__(self, gravity: float, depth_estimation_method: str) -> None:
-
-        super().__init__(gravity, depth_estimation_method)
-        self._travelled_distance = np.nan
-
-    @property
-    def travelled_distance(self) -> float:
-        """ :returns: the travelled distance during temporal lag """
-        return self._travelled_distance
-
-    @travelled_distance.setter
-    def travelled_distance(self, value: float) -> None:
-        self._travelled_distance = value
