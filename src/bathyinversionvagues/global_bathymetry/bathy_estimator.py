2774f71f9b782cc73a5e82ce1e17a53ba7a42e42
lasney <Yannick.Lasne@cnes.fr>
2022-03-29 10:08:42 +0000

    Modify bathy_estimator to allow for GeotiffDisToShoreProvider consideration

--- src/bathyinversionvagues/global_bathymetry/bathy_estimator.py
+++ src/bathyinversionvagues/global_bathymetry/bathy_estimator.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from ..data_providers.delta_time_provider import DeltaTimeProvider, NoDeltaTimeProviderError
 from ..data_providers.dis_to_shore_provider import (InfinityDisToShoreProvider, DisToShoreProvider,
-                                                    NetCDFDisToShoreProvider)
+                                                    NetCDFDisToShoreProvider, GeotiffDisToShoreProvider)
 from ..data_providers.gravity_provider import (LatitudeVaryingGravityProvider, GravityProvider,
                                                ConstantGravityProvider)
 from ..data_providers.roi_provider import (RoiProvider, VectorFileRoiProvider)
@@ -228,15 +228,20 @@ class BathyEstimator(ABC, BathyEstimatorParameters):
             self, provider_info: Optional[Union[Path, DisToShoreProvider]] = None) -> None:
         """ Sets the DisToShoreProvider to use with this estimator
 
-        :param provider_info: Either the DisToShoreProvider to use or a path to a netCDF file
-                           assuming a geographic NetCDF format.
+        :param provider_info: Either the DisToShoreProvider to use or a path to a netCDF or Geotiff file
+                           assuming a geographic NetCDF or Geotiff format.
         """
         if isinstance(provider_info, DisToShoreProvider):
             distoshore_provider = provider_info
         elif isinstance(provider_info, Path):
-            distoshore_provider = NetCDFDisToShoreProvider(provider_info, 4326,
-                                                           x_axis_label='lon',
-                                                           y_axis_label='lat')
+            if (Path(provider_info).suffix.lower() == '.nc'):
+                distoshore_provider = NetCDFDisToShoreProvider(provider_info, 4326,
+                                                               x_axis_label='lon',
+                                                               y_axis_label='lat')
+            elif (Path(provider_info).suffix.lower() == '.tif'):
+                distoshore_provider = GeotiffDisToShoreProvider(provider_info, 4326,
+                                                                x_axis_label='lon',
+                                                                y_axis_label='lat')
         else:
             # None or some other type, keep the current provider
             distoshore_provider = self._distoshore_provider
