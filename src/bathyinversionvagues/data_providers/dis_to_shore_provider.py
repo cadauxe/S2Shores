ab22839a3de9f7a0fc14e7e099dd5e630d4b2776
lasney <Yannick.Lasne@cnes.fr>
2022-03-29 08:30:53 +0000

    Issue05 Starting with GeotiffDisToShoreProvider implementation

--- src/bathyinversionvagues/data_providers/dis_to_shore_provider.py
+++ src/bathyinversionvagues/data_providers/dis_to_shore_provider.py
@@ -10,6 +10,7 @@ from typing import Optional, Any
 
 import xarray as xr  # @NoMove
 import numpy as np
+import rioxarray as rio
 
 from ..image.image_geometry_types import PointType
 
@@ -78,6 +79,46 @@ class NetCDFDisToShoreProvider(DisToShoreProvider):
         """
         if self._distoshore_xarray is None:
             self._distoshore_xarray = xr.open_dataset(self._distoshore_file_path)
+        provider_point = self.transform_point(point, 0.)
+        kw_sel = {self._x_axis_label: provider_point[0],
+                  self._y_axis_label: provider_point[1],
+                  'method': 'nearest'}
+        distance_xr_dataset = self._distoshore_xarray.sel(**kw_sel)
+        return float(distance_xr_dataset['disToShore'].values)
+
+
+class GeotiffDisToShoreProvider(DisToShoreProvider):
+    """ A DistToShoreProvider which provides the distance to shore when it is stored in a
+    'disToShore' layer of a Geotiff file.
+    """
+
+    # FIXME: EPSG code needed because no SRS retrieved from the NetCDF file at this time.
+    def __init__(self, distoshore_file_path: Path, distoshore_epsg_code: int,
+                 x_axis_label: str, y_axis_label: str) -> None:
+        """ Create a GeotiffDisToShoreProvider object and set necessary informations
+
+        :param distoshore_file_path: full path of a geotiff file containing the distance to shore
+                                     to be used by this provider.
+        :param distoshore_epsg_code: the EPSG code of the SRS used in the Geotiff file.
+        :param x_axis_label: Label of the x axis of the dataset ('x' or 'lon' for instance)
+        :param y_axis_label: Label of the y axis of the dataset ('y' or 'lat' for instance)
+        """
+        super().__init__()
+        self.provider_epsg_code = distoshore_epsg_code
+        self._x_axis_label = x_axis_label
+        self._y_axis_label = y_axis_label
+
+        self._distoshore_file_path = distoshore_file_path
+        self._distoshore_xarray: Optional[Any] = None
+
+    def get_distoshore(self, point: PointType) -> float:
+        """ Provides the distance to shore of a point in kilometers.
+
+        :param point: a tuple containing the X and Y coordinates in the SRS of the client
+        :returns: the distance to the nearest shore (km)
+        """
+        if self._distoshore_xarray is None:
+            self._distoshore_xarray = rio.open_rasterio(self._distoshore_file_path)
         provider_point = self.transform_point(point, 0.)
         kw_sel = {self._x_axis_label: provider_point[0],
                   self._y_axis_label: provider_point[1],
