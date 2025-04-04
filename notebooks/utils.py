0d0c6c641512d2f6f0deca75a1576cae0f039cb5
Geoffrey Paris <geoffrey.paris@mpdata.fr>
2025-04-04 16:29:21 +0200

    Use functions to hide start-up and post estimation statements

---
+++ notebooks/utils.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+from typing import Any
+import yaml
+
+from shapely.geometry import Point
+from xarray import Dataset
+
+from s2shores.bathylauncher.products.geotiff_product import GeoTiffProduct
+from s2shores.bathylauncher.products.s2_image_product import S2ImageProduct
+from s2shores.bathylauncher.bathy_launcher import BathyLauncher
+from s2shores.global_bathymetry.bathy_estimator import BathyEstimator
+from s2shores.global_bathymetry.ortho_bathy_estimator import OrthoBathyEstimator
+from s2shores.image.ortho_sequence import OrthoSequence
+from s2shores.data_model.estimated_points_bathy import EstimatedPointsBathy
+from s2shores.local_bathymetry.local_bathy_estimator import LocalBathyEstimator
+
+import s2shores
+
+def initialize_sequential_run(
+        product_path: Path,
+        config_path: Path,
+        point: Point,
+) -> tuple[BathyEstimator, OrthoBathyEstimator, OrthoSequence, dict[str, Any]]:
+    bathy_launcher = BathyLauncher(cluster=None, sequential_run=True)
+    bathy_estimator = initialize_bathy_estimator(
+        bathy_launcher=bathy_launcher,
+        product_path=product_path,
+        output_path=...,
+        config_path=config_path,
+        point=point,
+    )
+    ortho_bathy_estimator = initialize_ortho_bathy_estimator(bathy_estimator)
+    ortho_sequence = build_ortho_sequence(ortho_bathy_estimator)
+
+    return (
+        bathy_estimator,
+        ortho_bathy_estimator,
+        ortho_sequence,
+        bathy_estimator._waveparams,
+    )
+
+
+def initialize_bathy_estimator(
+        bathy_launcher: BathyLauncher,
+        product_path: Path,
+        output_path: Path,
+        config_path: Path,
+        point: Point,
+) -> BathyEstimator:
+    match product_path.suffix:
+        case ".tif":
+            product_cls = GeoTiffProduct
+        case ".s2":
+            product_cls = S2ImageProduct
+        case _:
+            raise ValueError(
+                "Product file type not recognized. Please use a geotiff or S2 file."
+            )
+
+    bathy_estimator = bathy_launcher.add_product(
+        product_path.stem,
+        product_path,
+        product_cls,
+        output_path,
+        read_config(config_path),
+        nb_subtiles=1,
+    )
+
+    bathy_estimator.set_delta_time_provider()
+
+    bathy_estimator.create_subtiles()
+    bathy_estimator.set_debug_samples([point])
+    bathy_estimator._debug_sample = True
+    return bathy_estimator
+
+
+def read_config(config_path: Path) -> dict[str, Any]:
+    with config_path.open() as file:
+        wave_params = yaml.load(file, Loader=yaml.FullLoader)
+
+    if 'CHAINS_VERSIONS' not in wave_params:
+        wave_params['CHAINS_VERSIONS'] = f's2shores : {s2shores.__version__}'
+
+    return wave_params
+
+
+def initialize_ortho_bathy_estimator(
+        bathy_estimator: BathyEstimator,
+):
+    subtile = bathy_estimator.subtiles[0]
+    return OrthoBathyEstimator(bathy_estimator, subtile)
+
+
+def build_ortho_sequence(
+        ortho_bathy_estimator: OrthoBathyEstimator,
+):
+    estimation_point = ortho_bathy_estimator.parent_estimator._debug_samples[0]
+    window = ortho_bathy_estimator.sampled_ortho.window_extent(estimation_point)
+
+    ortho_sequence = OrthoSequence(ortho_bathy_estimator.parent_estimator.delta_time_provider)
+    for frame_id in ortho_bathy_estimator.parent_estimator.selected_frames:
+        ortho_sequence.append_image(
+            ortho_bathy_estimator.sampled_ortho.read_frame_image(frame_id),
+            frame_id,
+        )
+    
+    return ortho_sequence.extract_window(window)
+
+
+def build_dataset(
+        bathy_estimator: BathyEstimator,
+        ortho_bathy_estimator: OrthoBathyEstimator,
+        local_bathy_estimator: LocalBathyEstimator,
+) -> Dataset:
+    estimated_bathy = EstimatedPointsBathy(
+        1,
+        ortho_bathy_estimator.sampled_ortho.ortho_stack.acquisition_time,
+    )
+    estimated_bathy.store_estimations(0, local_bathy_estimator.bathymetry_estimations)
+
+    dataset = estimated_bathy.build_dataset(
+        bathy_estimator.layers_type,
+        bathy_estimator.nb_max_wave_fields,
+    )
+
+    dataset = dataset.assign(spatial_ref=bathy_estimator._ortho_stack.build_spatial_ref())
+
+    # necessary to have a correct georeferencing
+    if 'x' in dataset.coords:  # only if output_format is GRID
+        dataset.x.attrs['standard_name'] = 'projection_x_coordinate'
+        dataset.y.attrs['standard_name'] = 'projection_y_coordinate'
+
+    infos = bathy_estimator.build_infos()
+    infos.update(bathy_estimator._ortho_stack.build_infos())
+    for key, value in infos.items():
+        dataset.attrs[key] = value
+
+    return dataset
